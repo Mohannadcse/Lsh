c6562761ec7239965e222c36b08e6882eca534a7
root <root@cse-jcui-10>
2017-03-02 10:02:39 -0600

    first

---
+++ src/com/bruno/App.java
@@ -0,0 +1,15 @@
+package com.bruno;
+
+import com.bruno.parser.MyParser;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		String pathFile = "/home/bsilva/Desktop/F16PA2/space-9.txt";
+		
+		MyParser.parseIt(pathFile);
+		
+	}
+	
+}
